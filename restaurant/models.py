632153dcf726fe390cb624d5db8564d0e2fb31f0
Pavlo Dumych <pavlo.dumych@gmail.com>
2022-06-16 18:28:10 +0300

    add class Reataurant

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Restaurant(models.Model):
+    name = models.CharField(max_length=256)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/restaurants')
+    kitchen_style = models.CharField(max_length=50)
+    location = models.CharField(max_length=150)
