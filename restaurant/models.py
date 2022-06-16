7e6ed6247cece81d7563a7494e34af6b56bee062
Pavlo Dumych <pavlo.dumych@gmail.com>
2022-06-16 18:53:31 +0300

    Add str to Restaurant model

--- restaurant/models.py
+++ restaurant/models.py
@@ -7,3 +7,6 @@ class Restaurant(models.Model):
     image = models.ImageField(upload_to='images/restaurants')
     kitchen_style = models.CharField(max_length=50)
     location = models.CharField(max_length=150)
+    
+    def __str__(self) -> str:
+        return f"{self.name} - {self.location}"
